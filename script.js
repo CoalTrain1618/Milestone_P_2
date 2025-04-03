7b34eae5d13616dc3ad8e365c82375361bbd6477
CoalTrain1618 <wallacegreen18@gmail.com>
2025-04-03 18:36:15 +0100

    Added event listner for card clicks for each card

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll(".card");
     let flippedCards = [];
     let matchedPairs = 0;
+
+    // Event listener for Card click/press
+    cards.forEach(card => {
+        card.addEventListener('click', (event) => {
+            
+        });
+    });
 });
