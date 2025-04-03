0910f691c1711688e1153644835473e14df97f1c
CoalTrain1618 <wallacegreen18@gmail.com>
2025-04-03 18:41:20 +0100

    Added conditional statement & funtionality to add 'flip' class to activated card

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // Event listener for Card click/press
     cards.forEach(card => {
         card.addEventListener('click', (event) => {
-            
+            const clickedCard = event.currentTarget;
+
+            if (flippedCards.length >= 2 || clickedCard.classList.contains('flip')) return;
+
+            clickedCard.classList.add('flip'); // Adds class to activated card
+            flippedCards.push(clickedCard);
         });
     });
 });
